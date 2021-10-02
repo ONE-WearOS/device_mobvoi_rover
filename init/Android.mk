bbf041861b1ce72966bfe1293b20048f6c5725b0
BadWolf-Alfa <marco@lunawolf.es>
2021-10-02 15:03:10 +0200

    Reworked

--- init/Android.mk
+++ init/Android.mk
@@ -1,13 +1,11 @@
 #
-# Copyright (C) 2020 The Android Open Source Project
-# Copyright (C) 2020 The TWRP Open Source Project
-# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2020 TeamWin Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,5 +14,15 @@
 # limitations under the License.
 #
 
-LOCAL_PATH := device/mobvoi/rover
+LOCAL_PATH := $(call my-dir)
 
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_C_INCLUDES := \
+    system/core/base/include \
+    system/core/init
+LOCAL_SRC_FILES := init_rover.cpp
+LOCAL_MODULE := libinit_rover
+
+include $(BUILD_STATIC_LIBRARY)
